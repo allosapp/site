f28f6f43d92177919a71d03a2e2ec46e9e4f5e1f
Evan Peterson <epeterson@rhumbix.com>
2025-12-29 00:33:41 -0800

    Landing page for Peak partnership

---
+++ src/js/account/peak.js
@@ -0,0 +1,39 @@
+import {
+  config as firebaseConfig,
+} from "../modules/firebase.js";
+import { runOnLoad } from "../modules/util.js";
+
+runOnLoad(() => {
+  /**
+   * This page uses global firebase and firebaseui objects defined globally via imported scripts in the HTML file,
+   * because FirebaseUI does not yet work with the modular Firebase packages.
+   */
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const ui = new firebaseui.auth.AuthUI(auth);
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      localStorage.setItem("utm_campaign", "peak");
+      window.location.replace("/account/profile");
+    }
+  });
+
+  ui.start("#firebaseui-auth-container", {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      {
+        provider: "apple.com",
+      },
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function() {
+        localStorage.setItem("utm_campaign", "peak");
+        window.location.replace("/account/profile/");
+        return false;
+      },
+    },
+  });
+});
